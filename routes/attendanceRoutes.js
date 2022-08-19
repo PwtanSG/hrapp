9d91f5f56cbe45393ebbabae836d59a2224a83dd
pwtansg <pwtan.edu@gmail.com>
2022-08-19 23:41:40 +0800

    Add attendance clock in route

--- routes/attendanceRoutes.js
+++ routes/attendanceRoutes.js
@@ -5,6 +5,7 @@ const { protect } = require('../middleware/authMiddleware')
 
 router.get('/' ,protect, getAttendances)
 router.post('/',protect, createAttendances)
+router.post('/clockin',protect, createAttendances)
 router.put('/clockout', protect,clockOutAttendances)
 router.put('/:id', protect,updateAttendances)
 router.delete('/:id', protect, deleteAttendances)
